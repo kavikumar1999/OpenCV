eba9c6f9c49c06b48ef6addbf0519383b353207f
Kavikumar M <96039379+kavikumar1999@users.noreply.github.com>
2022-09-26 14:16:17 +0530

    Delete FlippingSingleImage.py

--- FlippingSingleImage.py
+++
@@ -1,9 +0,0 @@
-import cv2
-image=cv2.imread("Resources\sample.jpg")
-
-Vflip=cv2.flip(image,0)#Vertically Flipped
-Hflip=cv2.flip(image,1)#Horizontally Flipped
-cv2.imshow("Original Image",image)
-cv2.imshow("Vertically Flipped Image",Vflip)
-cv2.imshow("Horizontally Flipped Image",Hflip)
-cv2.waitKey(0)
